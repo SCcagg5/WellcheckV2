7064b4454cf59782231c7a4e2ff9d870969c136f
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-03-26 05:38:36 +0100

    adding contact

--- landing/front/post.php
+++ landing/front/post.php
@@ -1,4 +1,6 @@
 <?php
-$message = htmlspecialchars($_POST["email"]) . ' | ' . htmlspecialchars($_POST["message"]);
-mail("eliot.courtel@wanadoo.fr", "Wellcheck - Contact", $message);
+$message = "| " . htmlspecialchars($_POST["email"]) . " | " . htmlspecialchars($_POST["message"]) . " |";
+$fp = fopen('/result/contact.txt', 'a+');
+fwrite($fp, str_replace("\n","",$message) ."\n");
+fclose($fp);
 ?>
